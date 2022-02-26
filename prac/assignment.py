4e1e817e9b9c400cf60df87ccc0274df24aac823
Munerin22 <sg.7scy@gmail.com>
2022-02-26 21:40:33 +0900

    update prac assignment_type,id

--- prac/assignment.py
+++ prac/assignment.py
@@ -13,3 +13,12 @@ print("3. Hey {} How are you doing?".format(name))
 #fstring
 # print(f"{hello} {world}")
 # print(f"3. Hey {name} How are you doing?")
+
+#built in function
+#type()
+hello_type = type(1.3)
+print(hello_type)
+
+#id()
+hello_id = id("hello")
+print(hello_id)
