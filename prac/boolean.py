71603154e71c310e838b76a07a03c2d85c19fcca
Munerin22 <sg.7scy@gmail.com>
2022-02-27 19:20:42 +0900

    数値型、エラー、ブーリアンの学習

---
+++ prac/boolean.py
@@ -0,0 +1,9 @@
+#Boolean型：True(真)とFalse(偽)
+print(True)
+print(False)
+
+barance = 1000
+withdrawal = 500
+
+print(barance < withdrawal)
+print(True + True)
---
+++ prac/error.py
@@ -0,0 +1,9 @@
+#result = 100 / 0    #バグ bug <-修正すること：デバッグ debug
+#ゼロエラー
+#print("hello world")
+
+#result = "1" + 3
+#print(result)
+#タイプエラー
+
+#エラーが出た際はメッセージを読んで解決すること
