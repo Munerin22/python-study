15ddb25935195eac7270fc461f6d1dae39206ac2
Munerin22 <sg.7scy@gmail.com>
2022-05-10 06:46:39 +0900

    moduleの学習開始

---
+++ module/mymodule.py
@@ -0,0 +1,9 @@
+myvariable = "This is global variable"
+
+
+def myfunc():
+    print("This is my function!!")
+
+# _(アンダースコア)を入れた関数はモジュール内でしか使用しない意思表示
+def _anotherfunc():
+    print("This is another function!!")
