671c504d16799b25645a3d160bc6778765e32e3a
Munerin22 <sg.7scy@gmail.com>
2022-05-19 23:18:42 +0900

    __name__ and built in module study

--- module/mymodule.py
+++ module/mymodule.py
@@ -7,3 +7,8 @@ def myfunc():
 # _(アンダースコア)を入れた関数はモジュール内でしか使用しない意思表示
 def _anotherfunc():
     print("This is another function!!")
+
+# moduleを開発する際にインポートによる不必要な実行を防ぐ
+if __name__ == "__main__":
+    print("This is mymodule")
+    print(f"mymodule.__name__: {__name__}")
